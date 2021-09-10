fb93938991131d2b1a3a8f45985f70ef2cca53ea
EstevenMart <emartine0829@gmail.com>
2021-09-09 21:11:07 -0500

    protoType

---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () =>(
+
+    <div>
+        <header>
+            <h1>Aplicacion</h1>
+        </header>
+
+    </div>
+
+);
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,36 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import NewComponent from './NewComponent';
+import Header from './Header';
+import Footer from './Footer';
+import ComponentePrueba from './ComponentePrueba';
 
-ReactDOM.render(
+/* ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  document.getElementById('root2')
+);
+
+ReactDOM.render(
+  <NewComponent/>,
   document.getElementById('root')
+); */
+
+ReactDOM.render(
+  <Header/>,
+  document.getElementById('root3')
+);
+
+/* ReactDOM.render(
+  <Footer/>,
+  document.getElementById('footer')
+); */
+
+ReactDOM.render(
+  <ComponentePrueba genero="F"/>,
+  document.getElementById('componentePrueba')
 );
 
 // If you want to start measuring performance in your app, pass a function
